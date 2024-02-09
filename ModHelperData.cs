c03cca6b444025707989f2b1f693660c40b4eb9c
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2024-02-10 00:43:15 +0100

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+﻿namespace AllParagonCombined;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "40.0";
+    public const string Version = "1.0.0";
+    public const string Name = "The Great Comeback Trust me";
+
+    public const string Description = "All paragon combined 100% not chaos";
+
+    public const string RepoOwner = "Mattcy1"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "Allparagon"; // TODO add your repo name here, also in the download url in README.md
+}
